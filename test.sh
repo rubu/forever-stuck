6c633143dab5ee10494fb86e047c7933014a9f68
Rūdolfs Bundulis <rudolfs.bundulis@gmail.com>
2024-07-11 11:25:21 +0300

    route forever logs and pids to a local folder inside the repo

--- test.sh
+++ test.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
-npx forever start --uid test -a --killSignal SIGTERM --minUptime 2000 --spinSleepTime 10000 src/index.js
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+
+# the line below seems to work
+# npx forever start --uid test -a --killSignal SIGTERM --minUptime 2000 -l ${SCRIPT_DIR}/.forever/test.log --pidFile ${SCRIPT_DIR}/.forever/index.js.pid src/index.js
+# the line below seems to cause forever stopall to get stuck
+npx forever start --uid test -a --killSignal SIGTERM --minUptime 2000 --spinSleepTime 10000 -l ${SCRIPT_DIR}/.forever/test.log --pidFile ${SCRIPT_DIR}/.forever/index.js.pid src/index.js
 
 npx forever list
 
-tail -f ~/.forever/test.log &
+tail -f ${SCRIPT_DIR}/.forever/test.log &
 
 sleep 5
 
-npx forever stopall
+npx forever stopall &
+stopall_pid=$!
+echo ${stopall_pid} > stopall.pid
+echo "forever stopall pid: ${stopall_pid}"
+wait $stopall_pid
